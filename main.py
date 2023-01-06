0f508d6bb7db41f3a52bb00844bb1d1f2e443b7d
Yifinity <yifan99.jiang@gmail.com>
2023-01-06 18:20:22 -0500

    Make runGame function

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ cursor = conn.cursor()
 pygame.init()
 # Screen Variables
 introduction = True
-running = True
+
 FrameHeight = 800
 FrameWidth = 1300
 screen = pygame.display.set_mode((FrameWidth, FrameHeight))
@@ -22,7 +22,7 @@ bg = pygame.transform.scale(bg, (FrameWidth, FrameHeight))
 clock = pygame.time.Clock()
 
 # Length, Width of most of the screen
-scroll = 0
+
 
 color_Dict = {
     "white": (255, 255, 255),
@@ -41,79 +41,83 @@ geoUser = pygame.transform.rotozoom(geoUserBig, 0, 0.75)
 user = geoUser.get_rect()  # Creates a rectangular region - haha get rect
 user.center = 84, 350  # 116 - 136 on actual coordinates
 userXSpeed = 0  # Default Speeds are zero
-userYSpeed = 0
-userScore = 0  # Default score - duh
+# userYSpeed = 0
+# userScore = 0  # Default score - duh
 
 # Text Stuff
 pygame.font.init()  # initialize font
 font = pygame.font.Font('freesansbold.ttf', 32)
 scorefont = pygame.font.Font('freesansbold.ttf', 27)
 # Gravity variables
-gravity = 0  # Should start at zero.
-userGravity = 3
-downUp = 1  # Frames increase going down, so a positive downUp means going down.
-
-# Block Variables
-rectShape = Rect(0, 375, 500, 50)  # This should be a rectangle of 500 X 50
-rectX = 0  # The x speed of the rectangle.
-rectSpeed = -10  # Rectangle speed as it goes to the left
-# Start the player
 
 mixer.music.play()
 print("Jared says 'Hello world!'")  # My friend made me write this - also confirmation that this is working
 
-while running:
-    screen.blit(bg, (scroll, 0))
-    screen.blit(bg, (FrameWidth + scroll, 0))
-    scroll -= 4  # Shift to the left
-    if scroll == -FrameWidth:
+def runGame():
+    userGravity = 3
+    downUp = 1  # Frames increase going down, so a positive downUp means going down.
+    running = True
+    scroll = 0
+    userScore = 0  # Default score - duh
+
+    # Block Variables
+    rectShape = Rect(0, 375, 500, 50)  # This should be a rectangle of 500 X 50
+    rectX = 0  # The x speed of the rectangle.
+    rectSpeed = -10  # Rectangle speed as it goes to the left
+    # Start the player
+    while running:
+        screen.blit(bg, (scroll, 0))
         screen.blit(bg, (FrameWidth + scroll, 0))
-        scroll = 0
-    # screen.(color_Dict["backGround"])  # Requires A tuple
-    pygame.draw.rect(screen, color_Dict["yellow"], rectShape)  # Draws the rectangle
-    screen.blit(geoUser, user)  # merge the image and the rect together
+        scroll -= 4  # Shift to the left
+        if scroll == -FrameWidth:
+            screen.blit(bg, (FrameWidth + scroll, 0))
+            scroll = 0
+        # screen.(color_Dict["backGround"])  # Requires A tuple
+        pygame.draw.rect(screen, color_Dict["yellow"], rectShape)  # Draws the rectangle
+        screen.blit(geoUser, user)  # merge the image and the rect together
 
-    score = font.render("Score: " + str(userScore), True, color_Dict["white"], None)
-    scoreRect = score.get_rect()
-    screen.blit(score, scoreRect)  # Merge text and rect
+        score = font.render("Score: " + str(userScore), True, color_Dict["white"], None)
+        scoreRect = score.get_rect()
+        screen.blit(score, scoreRect)  # Merge text and rect
 
-    # Rectangle Function
-    if rectShape.right < 0:  # When rectangle hits the end - the end of a level.
-        rectShape = Rect(1250, 375, 1200, 40)
-        userScore = userScore + 1  # Increase the score.
-    else:
-        rectX = rectSpeed
+        # Rectangle Function
+        if rectShape.right < 0:  # When rectangle hits the end - the end of a level.
+            rectShape = Rect(1250, 375, 1200, 40)
+            userScore = userScore + 1  # Increase the score.
+        else:
+            rectX = rectSpeed
 
-    if (user.right <= rectShape.right and user.left >= rectShape.left) and (user.bottom == rectShape.top or user.top == rectShape.bottom):
-        gravity = 0
-    else:
-        gravity = userGravity * downUp  # Gravity concept
+        if (user.right <= rectShape.right and user.left >= rectShape.left) and (user.bottom == rectShape.top or user.top == rectShape.bottom):
+            gravity = 0
+        else:
+            gravity = userGravity * downUp  # Gravity concept
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                downUp *= -1  # Flip gravity
-                # downUpHolder *= -1
-    if user.bottom > FrameHeight or user.top < 0:
-        running = not running
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    downUp *= -1  # Flip gravity
+                    # downUpHolder *= -1
+        if user.bottom > FrameHeight or user.top < 0:
+            running = not running
 
-    userYSpeed = gravity
-    rectShape.move_ip(rectX, 0)
-    user.move_ip(userXSpeed, userYSpeed)  # Move
-    pygame.display.update()
-    clock.tick(200)
+        userYSpeed = gravity
+        rectShape.move_ip(rectX, 0)
+        user.move_ip(userXSpeed, userYSpeed)  # Move
+        pygame.display.update()
+        clock.tick(200)
+    return userScore
 
+
+userEndScore = runGame()
 mixer.music.stop()  # Stop Music
-
-
 scoreBoard = Rect(650, 400, 500, 500)  # Default rectangle
 scoreBoard.center = (650, 400)
 pygame.draw.rect(screen, color_Dict["backGround"], scoreBoard)
 
 text = font.render('Nice Try!', True, color_Dict["white"])
-score = scorefont.render('Your Score: ' + str(userScore), True, color_Dict["white"])
+endscore = scorefont.render('Your Score: ' + str(userEndScore), True, color_Dict["white"])
 highScore = font.render('Top Scores: ', True, color_Dict["white"])
 
 cursor.execute('''SELECT * FROM scores ORDER BY score LIMIT 5''')
@@ -127,18 +131,8 @@ thirdPlace = scorefont.render(('3. ' + scores[2][1] + ": " + str(scores[2][0])),
 fourthPlace = scorefont.render(('4. ' + scores[1][1] + ": " + str(scores[1][0])), True, color_Dict["white"])
 fifthPlace = scorefont.render(('5. ' + scores[0][1] + ": " + str(scores[0][0])), True, color_Dict["white"])
 
-# textRect = text.get_rect()
-# scoreRect = score.get_rect()
-# highScoreRect = highScore.get_rect()
-
-# firstPlaceRect = firstPlace.get_rect()
-# secondPlaceRect = secondPlace.get_rect()
-# thirdPlaceRect = thirdPlace.get_rect()
-# fourthPlaceRect = fourthPlace.get_rect()
-# fifthPlaceRect = fifthPlace.get_rect()
-
 screen.blit(text, (scoreBoard.centerx - (text.get_width()//2), scoreBoard.top + 30))
-screen.blit(score, (scoreBoard.centerx - (score.get_width()//2), scoreBoard.top + 80))
+screen.blit(endscore, (scoreBoard.centerx - (endscore.get_width()//2), scoreBoard.top + 80))
 screen.blit(highScore, (scoreBoard.centerx - (highScore.get_width()//2), scoreBoard.top + 150))
 screen.blit(firstPlace, (scoreBoard.centerx - (firstPlace.get_width()//2), scoreBoard.top + 190))
 screen.blit(secondPlace, (scoreBoard.centerx - (secondPlace.get_width()//2), scoreBoard.top + 230))
